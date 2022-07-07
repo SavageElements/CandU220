45f4dbcd45767adf91bd095f725f5868569b04e1
J Rockwell Seebach <jrs@ironpinephoto.com>
2022-07-07 13:31:24 -0700

    220

---
+++ CandU220/Class220A.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CandU220
+{
+    class Class220A
+    {
+       public static void OperationONE (int X, int Y)
+        {
+            int numONE = 7;
+            Console.WriteLine(numONE + X);
+            Console.WriteLine(Y);
+
+        }
+    }
+}
--- CandU220/Program.cs
+++ CandU220/Program.cs
@@ -6,7 +6,17 @@ namespace CandU220
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Please enter a number");
+
+            int X = Convert.ToInt16(Console.ReadLine());
+
+            Console.WriteLine("Please Enter another number");
+
+            int Y = Convert.ToInt16(Console.ReadLine());
+
+            Class220A.OperationONE(X, Y);
+
+
         }
     }
 }
